13e05bf70eff7381008397e96c5c9ddb0e11c020
Scott Lowe <scottwithtwotees@gmail.com>
2018-11-20 11:29:24 -0500

    Add JAR release config

--- project.clj
+++ project.clj
@@ -1,23 +1,39 @@
-(defproject grafter/db "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject grafter/db "0.2.0-SNAPSHOT"
+  :description "Grafter SPARQL database query tools"
+  :url "https://github.com/Swirrl/grafter.db"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-beta5"]
+  :dependencies [[org.clojure/clojure "1.10.0-beta7"]
                  [org.clojure/core.cache "0.7.1"]
 
                  [com.taoensso/timbre "4.10.0"]
                  [duct/core "0.7.0-beta2"]
-                 [grafter "0.11.7"]])
+                 [grafter "0.11.7"]]
 
-:profiles
-{:dev [:project/dev :profiles/dev]
- :repl {:prep-tasks ^:replace ["javac" "compile"]}
+  :profiles
+  {:dev [:project/dev :profiles/dev]
+   :repl {:prep-tasks ^:replace ["javac" "compile"]}
 
- :test [:dev {:prep-tasks ^:replace ["javac" "compile"]
-              :jvm-opts ["-Xmx4g"]}]
+   :test [:dev {:prep-tasks ^:replace ["javac" "compile"]
+                :jvm-opts ["-Xmx4g"]}]
 
- :profiles/dev {}
- :project/dev {:jvm-opts [;; never collapse repeated stack traces in dev
-                          "-XX:-OmitStackTraceInFastThrow"]
-               :resource-paths ["test/resources"]}}
+   :profiles/dev {}
+   :project/dev {:jvm-opts [;; never collapse repeated stack traces in dev
+                            "-XX:-OmitStackTraceInFastThrow"]
+                 :resource-paths ["test/resources"]}}
+
+  :plugins [[s3-wagon-private "1.3.1"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["deploy" "swirrl-jars"]]
+
+  :repositories [["releases" {:sign-releases false
+                              :url "s3p://swirrl-jars/releases/"
+                              :username :env/AWS_ACCESS_KEY_ID
+                              :passphrase :env/AWS_SECRET_ACCESS_KEY
+                              :snapshots false}]
+                 ["snapshots" {:sign-releases false
+                               :url "s3p://swirrl-jars/snapshots/"
+                               :username :env/AWS_ACCESS_KEY_ID
+                               :passphrase :env/AWS_SECRET_ACCESS_KEY
+                               :releases false}]])
