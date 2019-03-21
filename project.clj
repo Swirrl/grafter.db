1aa63d56246e01cbaf88dc0b08ec5527e52b4a43
Scott Lowe <scottwithtwotees@gmail.com>
2019-03-20 21:52:38 -0400

    Make snapshot release to fix downstream permissions issue

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter.db "0.6.0"
+(defproject grafter.db "0.6.0-SNAPSHOT"
   :description "Grafter SPARQL database query tools"
   :url "https://github.com/Swirrl/grafter.db"
   :license {:name "Eclipse Public License"
