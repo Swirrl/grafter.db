58b1d8ce81d0a27412081fac4afca81ae293d963
Scott Lowe <scottwithtwotees@gmail.com>
2019-05-03 08:49:47 -0400

    Bump version to 0.8.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter.db "0.8.2"
+(defproject grafter.db "0.8.3"
   :description "Grafter SPARQL database query tools"
   :url "https://github.com/Swirrl/grafter.db"
   :license {:name "Eclipse Public License"
